cf8eb7cfdd8b61ce3830d5064d5df8aad638962d
Sarver Alam <97254593+Sarver2@users.noreply.github.com>
2024-10-07 21:16:01 +0530

    added more validation and responsiveness

--- app.js
+++ app.js
@@ -15,26 +15,29 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
     form.addEventListener("submit", (e) => {
       e.preventDefault();
-
-      formValidation();
-
-      const obj = {
-        fullName: fullName.value,
-        contact: contact.value,
-        address: address.value,
-        entryTime: entryTime.value,
-        exitTime: exitTime.value,
-        date: date.value,
-      };
-
-      visitorData.push(obj);
-
       if (formValidation()) {
+        formValidation();
+        const obj = {
+          fullName: fullName.value,
+          contact: contact.value,
+          address: address.value,
+          entryTime: entryTime.value,
+          exitTime: exitTime.value,
+          date: date.value,
+        };
+
+        visitorData.push(obj);
+
         localStorage.setItem("visitorData", JSON.stringify(visitorData));
-      }
 
-      // displayData();
-      if (formValidation()) {
+        // displayData();
+
+        Swal.fire({
+          title: "success",
+          text: "visitor detail added!",
+          icon: "success",
+        });
+
         form.reset();
       }
     });
@@ -69,128 +72,6 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
       return true;
     };
-
-    /*delete data */
-    // const deleteData = () => {
-    //   const trash = document.querySelectorAll(".delete-btn");
-    //   const tableBody = document.querySelector(".data");
-    //   const tableRow = document.querySelector("tr");
-
-    //   trash.forEach((btn, index) => {
-    //     btn.addEventListener("click", () => {
-    //       Swal.fire({
-    //         title: "Are you sure?",
-    //         text: "You won't be able to revert this!",
-    //         icon: "warning",
-    //         showCancelButton: true,
-    //         confirmButtonColor: "#3085d6",
-    //         cancelButtonColor: "#d33",
-    //         confirmButtonText: "Yes, delete it!",
-    //       }).then((result) => {
-    //         if (result.isConfirmed) {
-    //           visitorData.splice(index, 1);
-    //           localStorage.setItem("visitorData", JSON.stringify(visitorData));
-    //           // displayData();
-
-    //           Swal.fire({
-    //             title: "Deleted!",
-    //             text: "Your file has been deleted.",
-    //             icon: "success",
-    //           });
-    //         }
-    //       });
-    //     });
-    //   });
-    // };
-
-    /* edit date */
-    // const editData = () => {
-    //   const editBtn = document.querySelectorAll(".edit-btn");
-    //   const updtName = document.querySelector("#update-name");
-    //   const updtCnct = document.querySelector("#update-cnct");
-    //   const updtAdd = document.querySelector("#update-add");
-    //   const updtEnTime = document.querySelector("#update-enTime");
-    //   const updtExTime = document.querySelector("#update-exTime");
-    //   const updtDate = document.querySelector("#update-date");
-
-    //   editBtn.forEach((btn, index) => {
-    //     btn.addEventListener("click", () => {
-    //       updtName.value = btn.getAttribute("name");
-    //       updtAdd.value = btn.getAttribute("add");
-    //       updtEnTime.value = btn.getAttribute("enTime");
-    //       updtExTime.value = btn.getAttribute("exTime");
-    //       updtDate.value = btn.getAttribute("date");
-    //       updtCnct.value = btn.getAttribute("cnct");
-
-    //       const updateBtn = document.querySelector(".update-btn");
-    //       updateBtn.addEventListener("click", () => {
-    //         // formValidation();
-    //         const obj = {
-    //           fullName: updtName.value,
-    //           contact: updtCnct.value,
-    //           address: updtAdd.value,
-    //           entryTime: updtEnTime.value,
-    //           exitTime: updtExTime.value,
-    //           date: updtDate.value,
-    //         };
-
-    //         visitorData[index] = obj;
-
-    //         localStorage.setItem("visitorData", JSON.stringify(visitorData));
-
-    //         Swal.fire({
-    //           title: "Changes Saved!",
-    //           text: "data has been updated!",
-    //           icon: "success",
-    //         });
-
-    //         // displayData();
-    //         form.reset();
-    //       });
-    //     });
-    //   });
-    // };
-
-    // const updateData = () => {
-    //   editData();
-    // };
-
-    /*add data to table*/
-
-    // const displayData = () => {
-    //   const tableBody = document.querySelector(".data");
-    //   tableBody.innerHTML = "";
-    //   let rowCount = 1;
-    //   visitorData.forEach((visitor) => {
-    //     const tableRow = document.createElement("tr");
-    //     tableRow.classList.add("bodyData");
-
-    //     tableRow.innerHTML = `<td>${rowCount++}</td>
-    //                         <td>${visitor.fullName}</td>
-    //                         <td>${visitor.address}</td>
-    //                         <td>${visitor.entryTime}</td>
-    //                         <td>${visitor.exitTime}</td>
-    //                         <td>${visitor.date}</td>
-    //                         <td>${visitor.contact}</td>
-    //                         <td>
-    //                         <button name="${visitor.fullName}"
-    //                         add="${visitor.address}"
-    //                         enTime="${visitor.entryTime}"
-    //                         exTime="${visitor.exitTime}"
-    //                         date="${visitor.date}"
-    //                         cnct="${visitor.contact}"
-    //                         class="btn border-none p-0 edit-btn" type="button" data-bs-toggle="modal"  data-bs-target="#exampleModal"><i class="bi bi-pencil-square"></i> </button>
-    //                         <button class="btn border-none p-0 delete-btn"><i class="bi bi-trash"></i> </button>
-    //                         </td>`;
-
-    //     tableBody.appendChild(tableRow);
-    //   });
-
-    //   editData();
-    //   deleteData();
-    // };
-
-    // displayData();
   } else if (window.location.pathname.endsWith("record.html")) {
     //
     const updtName = document.querySelector("#update-name");
